aaeae6a9aa995440c45d1ec7adcd37a4ea3b2478
Alex <variuhin@yandex.ru>
2021-03-10 17:21:07 +0300

    Add pagination

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
 import {useState} from 'react'
 import {Table} from './components/Table.js'
 import {LoadData} from './components/LoadData.js'
+import {PagesNav} from './components/PagesNav.js'
 import {sortData} from './utils/utils.js'
 import './styles/App.css';
 
-const mock_data = [
-  {
-    id : 1,
-    firstName : 'A',
-    lastName: 'B',
-    email: 'example@example.com',
-    phone: '12345678'
-  },
-  {
-    id : 2,
-    firstName : 'C',
-    lastName: 'D',
-    email: 'example@example1.com',
-    phone: '123456789'
-  }
-]
-
 const App = () => {
-  const [data, setData] = useState(mock_data)
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+  const [rowsOnPage, setRowsOnPage] = useState(5)
 
   const sortCallback = (colObj, order) => {
     const sortedData = sortData(data, colObj.name, order)
     setData(sortedData)
   }
 
-  const loadCallback = (data) => {
-    setData(data)
+  const loadCallback = data => setData(data)
+
+  const pagesCount = Math.ceil(data.length/rowsOnPage)
+  const pageChangeCallback = newPage => {
+    if(newPage < 1 || newPage > pagesCount) return
+    setPage(newPage)
   }
+
+  const rowsNumChangeCallback = newRowsNum => setRowsOnPage(newRowsNum)
+
+  const from = (page-1)*rowsOnPage;
+  const dataToShow = data.slice(from, from + rowsOnPage)
 
   return (
     <div className="app">
       <LoadData loadCallback = {loadCallback} />
-      <Table data = {data} sortCallback = {sortCallback}/>
+      <Table data = {dataToShow} sortCallback = {sortCallback}/>
+      <PagesNav
+        page = {page} rowsOnPage = {rowsOnPage}
+        pagesCount = {pagesCount}
+        pageChangeCallback = {pageChangeCallback}
+        rowsNumChangeCallback = {rowsNumChangeCallback}
+      />
     </div>
   );
 }
---
+++ src/components/PagesNav.js
@@ -0,0 +1,48 @@
+export const PagesNav = ({
+    page, rowsOnPage, pagesCount, pageChangeCallback, rowsNumChangeCallback
+  }) => {
+  const onPageChange = (newPage) => pageChangeCallback(newPage)
+  const onRowsNumChange = newRowsNum => rowsNumChangeCallback(newRowsNum)
+
+  const prevEnable = (page !== 1)
+  const nextEnable = (page !== pagesCount)
+
+  return (
+    <div className = 'pages-nav-container d-flex'>
+      <button
+        className = 'btn btn-light'
+        onClick = {() => onPageChange(page - 1)}
+        disabled = {prevEnable ? null : true}
+      >
+        ◀
+      </button>
+      <nav className = 'd-flex w-100 justify-content-around'>
+        <form className = 'form-inline'>
+          <label>Page</label>
+          <input
+            className = 'form-control m-2'
+            type = 'number'
+            onChange = {e => onPageChange(+e.target.value)}
+            value = {page}
+          />
+          <label>of {pagesCount}</label>
+        </form>
+        <form className = 'form-inline'>
+          <label className = 'mr-2'>Rows on page:</label>
+          <select onChange = {e => onRowsNumChange(+e.target.value)}>
+            <option>5</option>
+            <option>10</option>
+            <option>50</option>
+          </select>
+        </form>
+      </nav>
+      <button
+        className = 'btn btn-light'
+        onClick = {() => onPageChange(page + 1)}
+        disabled = {nextEnable ? null : true}
+      >
+        ▶
+      </button>
+    </div>
+  )
+}
