79d2f89942169111d3b6aab4acfb7e47d59c9cf6
Alex <variuhin@yandex.ru>
2021-03-17 16:20:32 +0300

    Add tests for details component

---
+++ src/components/Details/Details.test.js
@@ -0,0 +1,26 @@
+import {render, fireEvent} from '@testing-library/react'
+import {Details} from './Details.js'
+import {mock_data} from '../../test/mock_data.js'
+
+describe('Details component tests', () => {
+  const mock_closeDetailsCallback = jest.fn()
+  const props = {
+    dataObj: {...mock_data[2]},
+    closeDetailsCallback: mock_closeDetailsCallback
+  }
+  test('renders properly', () => {
+    const container = render(<Details {...props} />)
+    expect(container).toMatchSnapshot()
+  })
+  test('renders properly without address and description', () => {
+    props.dataObj = {...mock_data[4]}
+    const container = render(<Details {...props} />)
+    expect(container).toMatchSnapshot()
+  })
+  test('Close button calls close callback function', () => {
+    render(<Details {...props} />)
+    const button = document.querySelector('.close')
+    fireEvent.click(button)
+    expect(mock_closeDetailsCallback).toHaveBeenCalledTimes(1)
+  })
+})
